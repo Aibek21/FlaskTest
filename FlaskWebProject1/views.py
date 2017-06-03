5721d890b9fe1fa460f9783f3181968496324e22
Aibek21 <aibekkuralbaev@gmail.com>
2017-06-03 11:50:04 +0600

    asd

--- FlaskWebProject1/views.py
+++ FlaskWebProject1/views.py
@@ -12,25 +12,25 @@ from flask import request
 from flask import url_for
 from FlaskWebProject1 import app
 # from models.azuretablestorage import get_gens
-from azure.storage.table import TableService, Entity
+# from azure.storage.table import TableService, Entity
 # from azure.storage.queue import QueueService
 
 
 
 
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
+# @app.errorhandler(404)
+# def not_found(error):
+#     return make_response(jsonify({'error': 'Not found'}), 404)
 
 
-@app.route('/todo/api/v1.0/generators', methods=['GET'])
-def get_generators():
-    table_service = TableService(account_name='kuralbayevday1', account_key='8onfJI8YYDJ9pB+SLOcFycntsRnL+iJm6y7R0mhTk8mSKZs9bXBo1v19bdwYOW4ts09440t6Gct2H2JtvjosRQ==')
-    generators = table_service.query_entities('generatorslist')
-    # print type(tasks)
-    # generators = get_gens()
-    return jsonify({'generators': map(make_public_generator, generators)})
-    # return "ok"
+# @app.route('/todo/api/v1.0/generators', methods=['GET'])
+# def get_generators():
+#     table_service = TableService(account_name='kuralbayevday1', account_key='8onfJI8YYDJ9pB+SLOcFycntsRnL+iJm6y7R0mhTk8mSKZs9bXBo1v19bdwYOW4ts09440t6Gct2H2JtvjosRQ==')
+#     generators = table_service.query_entities('generatorslist')
+#     # print type(tasks)
+#     # generators = get_gens()
+#     return jsonify({'generators': map(make_public_generator, generators)})
+#     # return "ok"
 
 
 
@@ -46,22 +46,22 @@ def get_generators():
 
 
 
-@app.route('/todo/api/v1.0/add_task', methods=['POST'])
-def create_task():
-    if not request.json or not 'generator' in request.json:
-        abort(400)
+# @app.route('/todo/api/v1.0/add_task', methods=['POST'])
+# def create_task():
+#     if not request.json or not 'generator' in request.json:
+#         abort(400)
 
 
-    # queue_service = QueueService(account_name='kuralbayevday1', account_key='8onfJI8YYDJ9pB+SLOcFycntsRnL+iJm6y7R0mhTk8mSKZs9bXBo1v19bdwYOW4ts09440t6Gct2H2JtvjosRQ==')
-    # queue_service.put_message('generatorqueue', request.json['generator'])
+#     # queue_service = QueueService(account_name='kuralbayevday1', account_key='8onfJI8YYDJ9pB+SLOcFycntsRnL+iJm6y7R0mhTk8mSKZs9bXBo1v19bdwYOW4ts09440t6Gct2H2JtvjosRQ==')
+#     # queue_service.put_message('generatorqueue', request.json['generator'])
 
-    return "ok", 201
+#     return "ok", 201
 
 
 
-def make_public_generator(generator):
-    new_generator = {}
-    for field in generator:
-            new_generator[field] = generator[field]
-    return new_generator
+# def make_public_generator(generator):
+#     new_generator = {}
+#     for field in generator:
+#             new_generator[field] = generator[field]
+#     return new_generator
 
