2b4f3400954a048d7bf62b313f81d045e20c0b90
Aibek21 <aibekkuralbaev@gmail.com>
2017-06-03 12:03:20 +0600

    asd

--- FlaskWebProject1/api.py
+++ FlaskWebProject1/api.py
@@ -12,13 +12,13 @@ from flask import request
 from flask import url_for
 from FlaskWebProject1 import app
 # from models.azuretablestorage import get_gens
-# from azure.storage import CloudStorageAccount
-# from azure.storage.table import TableService, Entity
-# from azure.storage.queue import QueueService
+from azure.storage import CloudStorageAccount
+from azure.storage.table import TableService, Entity
+from azure.storage.queue import QueueService
 
 
 
-# table_service = TableService(account_name='kuralbayevday1', account_key='8onfJI8YYDJ9pB+SLOcFycntsRnL+iJm6y7R0mhTk8mSKZs9bXBo1v19bdwYOW4ts09440t6Gct2H2JtvjosRQ==')
+table_service = TableService(account_name='kuralbayevday1', account_key='8onfJI8YYDJ9pB+SLOcFycntsRnL+iJm6y7R0mhTk8mSKZs9bXBo1v19bdwYOW4ts09440t6Gct2H2JtvjosRQ==')
 
 @app.errorhandler(404)
 def not_found(error):
